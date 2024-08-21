512c4ed2a5ebc813311b42deededa3e97b53f505
tengkuzainul <tengkuzainula04@gmail.com>
2024-08-21 16:45:59 +0700

    payment proccess

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -17,6 +17,25 @@
                 <p class="h3 text-dark text-center mb-3" style="font-weight: 700">Cart</p>
                 <div class="card shadow-lg">
                     <div class="card-body p-3">
+                        <div class="d-flex justify-content-start align-items-center gap-2">
+                            <img src="{{ asset('assets/image/empty-image.webp') }}" alt="..."
+                                class="img-thumbnail my-2 border-0 " width="50" height="50">
+                            <div class="d-flex flex-column">
+                                <p class="text-dark mb-0" style="font-weight: 700">Lorem, ipsum dolor.</p>
+                                <p class="text-primary mb-0"style="font-weight: 900">Rp. 15,000</p>
+                            </div>
+                            <div class="d-flex flex-column ms-4">
+                                <p class="text-danger mb-1" id="totalPayment">Rp, 30,000</p>
+                                <div class="d-flex gap-2">
+                                    <button class="btn btn-primary btn-sm" type="button" id="buttonPlus"><i
+                                            class="ti ti-minus"></i></button>
+                                    <input name="qty" type="text" class="form-control form-control-sm w-25"
+                                        min="1" size="1" inputmode="numeric" value="1">
+                                    <button class="btn btn-danger btn-sm" type="button" id="buttonPlus"><i
+                                            class="ti ti-plus"></i></button>
+                                </div>
+                            </div>
+                        </div>
                         <p class="mb-0">No items in cart</p>
                     </div>
                 </div>
